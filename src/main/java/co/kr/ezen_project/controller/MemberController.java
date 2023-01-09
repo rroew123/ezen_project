ed2581578289712ca52fb80c1a28ee1de646d198
ShinDongGeon <aa01087476786@gmail.com>
2023-01-09 12:39:02 +0900

    push test

--- src/main/java/co/kr/ezen_project/controller/MemberController.java
+++ src/main/java/co/kr/ezen_project/controller/MemberController.java
@@ -1,8 +1,12 @@
 package co.kr.ezen_project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemberController {
+	@RequestMapping(value = "home")
+	public void home() {
+	}
 
 }
