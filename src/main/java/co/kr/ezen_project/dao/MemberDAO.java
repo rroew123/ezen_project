1963f4fc4213c81b438453790c97fb31874041e6
hyeontae <y_sm0214@naver.com>
2023-01-09 10:35:07 +0900

    pom.xml에서 string-test의 scope제거 test 파일 생성 및 기본 테스트 context, dataSource, sqlSession, dao연결, mapper연결 및 정상 작동 확인

--- src/main/java/co/kr/ezen_project/dao/MemberDAO.java
+++ src/main/java/co/kr/ezen_project/dao/MemberDAO.java
@@ -1,5 +1,10 @@
 package co.kr.ezen_project.dao;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public interface MemberDAO {
+
+	String getTime();
 
 }
--- src/main/java/co/kr/ezen_project/dao/MemberDAOImpl.java
+++ src/main/java/co/kr/ezen_project/dao/MemberDAOImpl.java
@@ -1,5 +1,17 @@
 package co.kr.ezen_project.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MemberDAOImpl implements MemberDAO{
+	@Autowired
+	SqlSession session;
+	
+	@Override
+	public String getTime() {
+		return session.selectOne("co.kr.ezen_project.dao.MemberDAO.getTime");	//테스트용
+	}
 
 }
