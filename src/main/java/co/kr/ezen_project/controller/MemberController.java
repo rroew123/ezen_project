155ee09638db8a5e8c75a9ee38b52fa15da69e68
ShinDongGeon <aa01087476786@gmail.com>
2023-01-12 10:02:43 +0900

    컨트롤러 보내기 1

--- src/main/java/co/kr/ezen_project/controller/MemberController.java
+++ src/main/java/co/kr/ezen_project/controller/MemberController.java
@@ -5,8 +5,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemberController {
-	@RequestMapping(value = "home")
-	public void home() {
-	}
+	/* mypage 연결	마이페이지
+
+	 * customer 연결	고객센터
+	 * 
+	 * mypage_board 연결	마이페이지 내게시글
+	 * 
+	 * mypage_memDelete연결	마이페이지 회원탈퇴
+	 * 
+	 * mypage_memUpdate연결	마이페이지 회원정보수정
+
+	 * mypage_orderHis연결	마이페이지 마이페이지 주문내역
+	 * 
+	 * mypage_recentlyView연결	마이페이지 최근본상품
+	 * 
+	 * mypage_wishlist연결	마이페이지 관심상품
+	 * 
+	 * ntcBoard연결 공지사항
+	 * 
+	 */
+	 
 
 }
--- src/main/java/co/kr/ezen_project/controller/SangpumController.java
+++ src/main/java/co/kr/ezen_project/controller/SangpumController.java
@@ -4,5 +4,14 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class SangpumController {
-
+	/* sangpum 연결 상품페이지
+	 * sangpum_Info 상품 상세정보
+	 * sangpum_review 상품리뷰
+	 * sangpum_QnA 상품QnA
+	 * category 분류페이지
+	 * category_best 분류_인기
+	 * category_new 분류_신상
+	 * category_lowprice 분류_낮은가격
+	 * category_highprice 분류_높은가격 
+	*/
 }
