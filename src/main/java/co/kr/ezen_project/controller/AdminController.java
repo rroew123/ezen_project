125efea3f2b6ca9681a859091784cea338a38c20
hjikim17 <hjikim17@gmail.com>
2023-01-20 12:40:55 +0900

    오타 에러

--- src/main/java/co/kr/ezen_project/controller/AdminController.java
+++ src/main/java/co/kr/ezen_project/controller/AdminController.java
@@ -78,7 +78,7 @@ public class AdminController {
 		map.put("searchOption", searchOption);
 		map.put("keyword", keyword);
 		mav.addObject("map", map);
-		mav.setViewName("admin/adminMember");
+		mav.setViewName("/admin/adminMember");
 		return mav;
 	}
 	
